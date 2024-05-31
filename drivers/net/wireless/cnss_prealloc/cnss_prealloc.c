304b5ae9e332cb3c936ebbfa567299893a56612a
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-05-31 14:54:59 +0800

    treewide: Fix logspam with !IPC_LOGGING/!DEBUG_FS/!TRACEPOINTS
    Co-authored-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Co-authored-by: Adam W. Willis <return.of.octobot@gmail.com>
    Co-authored-by: kawaaii <kawaaii@nocturn9x.space>
    Co-authored-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>
    Co-authored-by: EmanuelCN <emanuelghub@gmail.com>
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Co-authored-by: CuriousNom <aryanbiswas7585@gmail.com>

--- drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
+++ drivers/net/wireless/cnss_prealloc/cnss_prealloc.c
@@ -22,7 +22,9 @@ static DEFINE_SPINLOCK(alloc_lock);
 #define PRE_ALLOC_DEBUGFS_DIR		"cnss-prealloc"
 #define PRE_ALLOC_DEBUGFS_FILE_OBJ	"status"
 
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *debug_base;
+#endif
 
 struct wcnss_prealloc {
 	int occupied;
@@ -299,7 +301,9 @@ static int __init wcnss_pre_alloc_init(void)
 static void __exit wcnss_pre_alloc_exit(void)
 {
 	wcnss_prealloc_deinit();
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(debug_base);
+#endif
 }
 
 module_init(wcnss_pre_alloc_init);
