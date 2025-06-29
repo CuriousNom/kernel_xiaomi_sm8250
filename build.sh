1fe7d2491db8dcc8eefe6972ac20994aef69db01
CuriousNom <aryanbiswas7585@gmail.com>
2025-06-29 08:34:52 +0530

    build: Simply build script
    * Only build for pipa

--- build.sh
+++ build.sh
@@ -1,208 +1,75 @@
 #!/bin/bash
 
-# Добавляем данные из настроек
-source ../settings.sh
+# Ensure the script exits on error
+set -e
 
-#
-# Создайте файл ../settings.sh если его у вас нет
-# Его содержание:
-#
-# export VERSION="1.x.x"
-# export BUILD=1
-# export PREFIX="e"
-# export DESC="description"
-# export DEVICE="alioth"
-# export TGTOKEN=bot_id
-# export LAST=last commit hash for generation changelog
-#
+TOOLCHAIN_PATH=$HOME/tc/bin
+GIT_COMMIT_ID=$(git rev-parse --short=8 HEAD)
 
-# Начало отсчета времени выполнения скрипта
-start_time=$(date +%s)
-
-# Удаление каталога "out", если он существует
-rm -rf out
-
-# Основной каталог
-MAINPATH=/home/timisong # измените, если необходимо
-
-# Каталог ядра
-KERNEL_DIR=$MAINPATH/kernel
-KERNEL_PATH=$KERNEL_DIR/kernel_xiaomi_sm8250
-
-git log $LAST..HEAD > ../changelog.txt
-BRANCH=$(git branch --show-current)
-
-# Каталоги компиляторов
-CLANG_DIR=$KERNEL_DIR/clang20
-GCC_ARM_DIR=$KERNEL_DIR/arm-linux-androideabi-4.9
-GCC_AARCH64_DIR=$KERNEL_DIR/aarch64-linux-android-4.9
-
-# Проверка и клонирование, если необходимо
-check_and_clone() {
-    local dir=$1
-    local repo=$2
-    local name=$3
-
-    if [ ! -d $dir ]; then
-        echo Папка $dir не существует. Клонирование $repo
-        cd $dir
-        git clone $repo $name
-    fi
-}
-
-check_and_wget() {
-    local dir=$1
-    local repo=$2
-
-    if [ ! -d $dir ]; then
-        echo Папка $dir не существует. Клонирование $repo
-        mkdir $dir
-        cd $dir
-        wget -O clang.tar.gz $repo
-        tar -zxvf clang.tar.gz
-        rm -rf clang.tar.gz
-        cd ../kernel_xiaomi_sm8250
-    fi
-}
-
-# Клонирование инструментов компиляции, если они не существуют
-check_and_wget $CLANG_DIR \
-    https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20250129-release/Clang-20.0.0git-20250129.tar.gz
-check_and_clone $GCC_ARM_DIR \
-    https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 \
-        arm-linux-androideabi-4.9
-check_and_clone $GCC_AARCH64_DIR \
-    https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 \
-        aarch64-linux-android-4.9
-
-# Каталог для сборки MagicTime
-MAGICTIME_DIR=$KERNEL_DIR/MagicTime-$DEVICE
-
-# Создание каталога MagicTime, если его нет
-if [ ! -d $MAGICTIME_DIR ]; then
-    mkdir -p $MAGICTIME_DIR
-    
-    # Проверка и клонирование Anykernel, если MagicTime не существует
-    if [ ! -d $MAGICTIME_DIR/Anykernel ]; then
-        git clone https://github.com/TIMISONG-dev/Anykernel.git \
-            $MAGICTIME_DIR/Anykernel
-        
-        # Перемещение всех файлов из Anykernel в MagicTime
-        mv $MAGICTIME_DIR/Anykernel/* $MAGICTIME_DIR/
-        
-        # Удаление папки Anykernel
-        rm -rf $MAGICTIME_DIR/Anykernel
-    fi
-else
-    # Если папка MagicTime существует, проверить наличие .git и удалить, если есть
-    if [ -d $MAGICTIME_DIR/.git ]; then
-        rm -rf $MAGICTIME_DIR/.git
-    fi
+if [ ! -d $TOOLCHAIN_PATH ]; then
+    echo "TOOLCHAIN_PATH [$TOOLCHAIN_PATH] does not exist."
+    echo "Please ensure the toolchain is there, or change TOOLCHAIN_PATH in the script to your toolchain path."
+    exit 1
 fi
 
-# Экспорт переменных среды
-if [ $DEVICE = pipa ]; then
-    IMGPATH=$MAGICTIME_DIR/kernels/Image
-    DTBPATH=$MAGICTIME_DIR/kernels/dtb
-    DTBOPATH=$MAGICTIME_DIR/kernels/dtbo.img
+echo "TOOLCHAIN_PATH: [$TOOLCHAIN_PATH]"
+export PATH="$TOOLCHAIN_PATH:$PATH"
+
+# Enable ccache for speed up compiling
+export CCACHE_DIR="$HOME/.cache/ccache_mikernel"
+export CC="ccache gcc"
+export CXX="ccache g++"
+export PATH="/usr/lib/ccache:$PATH"
+echo "CCACHE_DIR: [$CCACHE_DIR]"
+
+MAKE_ARGS="ARCH=arm64 O=out CC=clang LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip"
+
+# Check clang is existing.
+echo "[clang --version]:"
+clang --version
+
+# Export variables
+export KBUILD_BUILD_USER="aryan"
+export KBUILD_BUILD_HOST="curiousnom"
+export KBUILD_LAST_COMMIT=${GIT_COMMIT_ID}
+
+echo "Cleaning..."
+rm -rf out/
+rm -rf error.log
+
+echo "Cloning AnyKernel3 for packing kernel..."
+if [ -d "anykernel/.git" ]; then
+    echo "AnyKernel3 already cloned. Skipping."
 else
-    IMGPATH=$MAGICTIME_DIR/Image
-    DTBPATH=$MAGICTIME_DIR/dtb
-    DTBOPATH=$MAGICTIME_DIR/dtbo.img
+    rm -rf anykernel  # ensure clean state
+    git clone https://github.com/CuriousNom/AnyKernel3 -b pipa-mt --single-branch --depth=1 anykernel
 fi
 
-# Установка переменных PATH
-export PATH=$CLANG_DIR/bin:$GCC_AARCH64_DIR/bin:$GCC_ARM_DIR/bin:$PATH
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
-export KBUILD_BUILD_USER=TIMISONG
-export KBUILD_BUILD_HOST=timisong-dev
+    # ------------- Building for PIPA ---------------
+    echo "Clearing [out/] and building for PIPA....."
 
-# Запись времени сборки
-MAGIC_BUILD_DATE=$(date '+%Y-%m-%d_%H-%M-%S')
+    make $MAKE_ARGS pipa_defconfig vendor/xiaomi/magictime-common.config
 
-# Каталог для результатов сборки
-OUT_DIR=out
+    make $MAKE_ARGS -j$(nproc --all) 2> >(tee -a error.log >&2)
 
-# Конфигурация ядра
-make O="$OUT_DIR" \
-            ${DEVICE}_defconfig \
-            vendor/xiaomi/magictime-common.config
+    if [ -f "out/arch/arm64/boot/Image" ]; then
+        echo "The file [out/arch/arm64/boot/Image.gz] exists. AOSP Build successfully."
+    else
+        echo "The file [out/arch/arm64/boot/Image.gz] does not exist. Seems AOSP build failed."
+        exit 1
+    fi
 
-    # Компиляция ядра
-    make -j $(nproc) \
-                O="$OUT_DIR" \
-                CC="ccache clang" \
-                HOSTCC=gcc \
-                LD=ld.lld \
-                AS=llvm-as \
-                AR=llvm-ar \
-                NM=llvm-nm \
-                OBJCOPY=llvm-objcopy \
-                OBJDUMP=llvm-objdump \
-                STRIP=llvm-strip \
-                LLVM=1 \
-                LLVM_IAS=1 \
-                V=$VERBOSE 2>&1 | tee build.log
-                
+    rm -rf anykernel/kernels/
+    mkdir -p anykernel/kernels/
+    cp out/arch/arm64/boot/Image.gz anykernel/kernels/
+    cp out/arch/arm64/boot/dtb anykernel/kernels/
 
-# Предполагается, что переменная DTS установлена ранее в скрипте
-find $DTS -name '*.dtb' -exec cat {} + > $DTBPATH
-find $DTS -name 'Image' -exec cat {} + > $IMGPATH
-find $DTS -name 'dtbo.img' -exec cat {} + > $DTBOPATH
+    cd anykernel
+    ZIP_FILENAME=Kernel_MagicTime_AOSP_pipa_$(date +'%Y%m%d_%H%M%S')_anykernel3_${GIT_COMMIT_ID}.zip
+    zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
+    mv $ZIP_FILENAME ../
+    cd ..
 
-# Завершение отсчета времени выполнения скрипта
-end_time=$(date +%s)
-elapsed_time=$((end_time - start_time))
+    echo "Build for PIPA finished."
 
-cd "$KERNEL_PATH"
-
-# Проверка успешности сборки
-if grep -q -E "Ошибка 2|Error 2" build.log; then
-    cd $KERNEL_PATH
-    echo Ошибка: Сборка завершилась с ошибкой
-
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendMessage \
-    -d chat_id=@magictimekernel \
-    -d text="Ошибка в компиляции!" \
-    -d message_thread_id=38153
-
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel \
-    -F document=@./build.log \
-    -F message_thread_id=38153
-
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel \
-    -F document=@../changelog.txt \
-    -F message_thread_id=38153
-else
-    echo Общее время выполнения: $elapsed_time секунд
-    # Перемещение в каталог MagicTime и создание архива
-    cd $MAGICTIME_DIR
-    7z a -mx9 MagicTime-$DEVICE-$MAGIC_BUILD_DATE.zip * -x!*.zip
-    
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendMessage \
-    -d chat_id=@magictimekernel \
-    -d text="Компиляция завершилась успешно! Время выполнения: $elapsed_time секунд" \
-    -d message_thread_id=38153
-
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel \
-    -F document=@./MagicTime-$DEVICE-$MAGIC_BUILD_DATE.zip \
-    -F caption="MagicTime ${VERSION}${PREFIX}${BUILD} (${DESC}) branch: ${BRANCH}" \
-    -F message_thread_id=38153
-    
-    curl -s -X POST https://api.telegram.org/bot$TGTOKEN/sendDocument?chat_id=@magictimekernel \
-    -F document=@../changelog.txt \
-    -F caption="Latest changes" \
-    -F message_thread_id=38153
-
-    rm -rf MagicTime-$DEVICE-$MAGIC_BUILD_DATE.zip
-
-    BUILD=$((BUILD + 1))
-
-    cd $KERNEL_PATH
-    LAST=$(git log -1 --format=%H)
-
-    sed -i "s/LAST=.*/LAST=$LAST/" ../settings.sh
-    sed -i "s/BUILD=.*/BUILD=$BUILD/" ../settings.sh
-fi
+echo "Done. The flashable zip is: [./$ZIP_FILENAME]"
